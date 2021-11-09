221244e9be12283d5cb9da319d88920399299ef4
andreicristea <andrei.cristea@apicore.de>
2021-11-09 04:22:15 +0100

    feat: outer space for canvas

--- js/Maze.js
+++ js/Maze.js
@@ -5,8 +5,10 @@ import { MazeObject } from "./maze-objects/MazeObject.js";
 import { Position } from "./util/Position.js";
 
 class Maze {
-    static size = 7;
-    static rotations = [0, 90, 180, 270];
+    static MAZE_SIZE = 7;
+    static ROTATIONS = [0, 90, 180, 270];
+    static SIZE = 512;
+    static OUTER_SIZE = 100;
 
     #canvas;
     #canvasContext;
@@ -32,8 +34,8 @@ class Maze {
         this.#canvasContext = this.#canvas.getContext("2d");
         this.initCanvas();
 
-        this.#roomSize = (this.#canvas.width) / 7;
-        this.#rooms = Array(Maze.size).fill(Array(Maze.size).fill(false));
+        this.#roomSize = (Maze.SIZE) / Maze.MAZE_SIZE;
+        this.#rooms = Array(Maze.MAZE_SIZE).fill(Array(Maze.MAZE_SIZE).fill(false));
         this.#srcImages = srcImages;
         this.initRooms();
     }  
@@ -44,12 +46,12 @@ class Maze {
             map( (_, roomIndex) => {
                 if ((rowIndex + 1) % 2 === 1 && (roomIndex + 1) % 2 === 1) {
                     const randomImageIndex = Math.round(Math.random() * (this.#srcImages.length - 1));
-                    const randomRotation = Math.round(Math.random() * (Maze.rotations.length - 1));
+                    const randomRotation = Math.round(Math.random() * (Maze.ROTATIONS.length - 1));
                     return new MazeObject({
                         src: this.#srcImages[randomImageIndex],
-                        position: new Position(this.#roomSize, this.#roomSize, rowIndex * this.#roomSize, roomIndex * this.#roomSize), 
+                        position: new Position(this.#roomSize, this.#roomSize, (rowIndex * this.#roomSize) + Maze.OUTER_SIZE, roomIndex * this.#roomSize), 
                         canvasContext: this.#canvasContext,
-                        rotation: Maze.rotations[randomRotation]
+                        rotation: Maze.ROTATIONS[randomRotation]
                     })
                 }
                 return null;
@@ -57,13 +59,20 @@ class Maze {
     }
 
     initCanvas() {
-        this.#canvas.classList.add('game-canvas');
+        this.#canvas.height = Maze.SIZE;
+        this.#canvas.width = Maze.SIZE + 2 * Maze.OUTER_SIZE;
+        // this.#canvas.classList.add('game-canvas');
         // DRAW FIRST EMPTY
-        // this.#canvasContext.fillStyle = Colors.BACKGROUND_BLUE;
-        // this.#canvasContext.fillRect(0, 0, 100, this.#canvas.height);
+        this.#canvasContext.fillStyle = Colors.BACKGROUND_BLUE;
+        this.#canvasContext.fillRect(0, 0, Maze.OUTER_SIZE, this.#canvas.height);
 
         this.#canvasContext.fillStyle  = Colors.BACKGROUND_ORANGE;
-        this.#canvasContext.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
+        this.#canvasContext.fillRect(Maze.OUTER_SIZE, 0, this.#canvas.width, this.#canvas.height);
+
+        // this.#canvas.classList.add('game-canvas');
+        // DRAW SECOND EMPTY
+        this.#canvasContext.fillStyle = Colors.BACKGROUND_BLUE;
+        this.#canvasContext.fillRect(this.#canvas.width - Maze.OUTER_SIZE, 0, Maze.OUTER_SIZE, this.#canvas.height);
     }
 }
 
